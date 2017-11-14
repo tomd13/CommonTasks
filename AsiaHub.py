5ecd7fd5dbeb731e5379e12dc72564b875c0e4e1
tomd13 <thomas.davies2@gmail.com>
2017-11-15 07:53:22 +0800

    Add image finder to AsiaHub

--- AsiaHub.py
+++ AsiaHub.py
@@ -1,6 +1,6 @@
 import pyautogui
 
-pyautogui.PAUSE = 0.2
+pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = True
 
 # Get resolution of current screen
@@ -10,15 +10,11 @@ width, height = pyautogui.size()
 # http://pyautogui.readthedocs.io/en/latest/screenshot.html#the-locate-functions
 
 def add_instrument(instrument, group):
-    #pyautogui.rightClick(pyautogui.locateCenterOnScreen(r'C:\Users\Tom\PycharmProjects\CommonTasks\Test2.png'))
-    pyautogui.click(50, 400)
-    pyautogui.click(50, 400)
+    # Find instrument group on screen and add new instrument
+    pyautogui.rightClick(pyautogui.locateCenterOnScreen(r'C:\Users\Tom\PycharmProjects\CommonTasks\asiahubjpy.png'))
     pyautogui.typewrite('a')
     pyautogui.press('enter')
     pyautogui.typewrite(str(instrument))
     pyautogui.press('enter', 2, 0.2)
 
 add_instrument('3mL 22-Dec', '')
-
-pyautogui.locateCenterOnScreen(r'C:\Users\Tom\PycharmProjects\CommonTasks\Test2.png')
-#pyautogui.typewrite(['a', 'b', 'enter', 'left', 'X', 'Y'])
