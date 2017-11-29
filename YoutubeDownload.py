1cfb1e352acc54cb02d2298d1748177e812e2225
tomd13 <thomas.davies2@gmail.com>
2017-11-29 21:47:06 +0800

    Default to m4a download

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,7 +1,7 @@
 import os
 
 def download(youtube_link):
-    os.system('e: && cd E:\Books (Audio and eBooks)\youtube-dl && youtube-dl -f bestaudio "' + youtube_link + '"')
+    os.system(r'e: && cd E:\Books (Audio and eBooks)\youtube-dl && youtube-dl -f "bestaudio[ext=m4a]/best[ext=mp4]/best" "' + youtube_link + '"')
 
 text_file = open("E:\Books (Audio and eBooks)\youtube-dl\Youtube links.txt", "r")
 links = text_file.readlines()
