9f9a92df16b895580be9e939fe6c71b00e63ef8c
tdavies <thomas.davies2@gmail.com>
2018-04-16 15:27:58 +0800

    Add instrument to relevant group

--- AsiaHub.py
+++ AsiaHub.py
@@ -1,6 +1,9 @@
 import pyautogui
+import time
+import sys
 
-pyautogui.PAUSE = 0
+# 0.5 sec pause between pyautogui commands
+pyautogui.PAUSE = 0.5
 pyautogui.FAILSAFE = True
 
 # Get resolution of current screen
@@ -11,10 +14,29 @@ width, height = pyautogui.size()
 
 def add_instrument(instrument, group):
     # Find instrument group on screen and add new instrument
-    pyautogui.rightClick(pyautogui.locateCenterOnScreen(r'C:\Users\Tom\PycharmProjects\CommonTasks\asiahubjpy.png'))
-    pyautogui.typewrite('a')
-    pyautogui.press('enter')
-    pyautogui.typewrite(str(instrument))
-    pyautogui.press('enter', 2, 0.2)
+    if group == 'clearingswitch':
+        header_loc = pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\clearingswitch.png')
+    elif group == 'ccybasis':
+        header_loc = pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\singleccybasis.png')
+    elif group == 'sps':
+        header_loc = pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\sps.png')
 
-add_instrument('3mL 22-Dec', '')
+    if header_loc is None:
+        return print("Can't find group header - make sure IRODeal is visible")
+    else:
+        print(header_loc)
+
+    pyautogui.rightClick(header_loc)
+    print(pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\add2.png'))
+    pyautogui.click(pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\add2.png'))
+    pyautogui.typewrite(str(instrument), 0.05)
+    time.sleep(1)
+    print(pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\ok2.png'))
+    pyautogui.click(pyautogui.locateCenterOnScreen(r'C:\Users\tdavies\PycharmProjects\untitled\ok2.png'))
+
+
+def add_instrument_test(instrument, group):
+    print(instrument)
+    print(group)
+
+add_instrument(" ".join(sys.argv[1:-1]), "".join(sys.argv[-1:]))
